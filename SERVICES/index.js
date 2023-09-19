d24c20b2b469cf2c72ad66c8cdbefaddcce96489
borisbecco <borisbecco12@gmail.com>
2023-09-19 01:16:03 -0300

    psuh

--- SERVICES/index.js
+++ SERVICES/index.js
@@ -292,13 +292,3 @@ document.getElementById("toggleButton").addEventListener("click", function () {
   toggleOverlay();
 });
 
-
-
-$(document).ready(function() {
-  $(".wwd-links-container").click(function() {
-    // Obtén la URL del enlace contenido en este contenedor
-    var url = $(this).find(".wwd-link").attr("href");
-    // Redirige a la URL
-    window.location.href = url;
-  });
-});
